cd95b7036df9790f79818d3c82ba134b47085eb5
Suparna A K <suparnaak@gmail.com>
2025-07-05 15:55:33 +0530

    vision key updated

--- server/utils/visionClient.js
+++ server/utils/visionClient.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const client = new vision.ImageAnnotatorClient({
-  keyFilename: process.env.GOOGLE_CREDENTIALS_JSON,
+  keyFilename: /* process.env.GOOGLE_CREDENTIALS_JSON, */'/etc/secrets/google-service-account.json'
 });
 
 export default client;
