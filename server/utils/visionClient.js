afd55e5c3766d70ff6fb2517ec5356c64e789e35
Suparna A K <suparnaak@gmail.com>
2025-07-05 15:23:25 +0530

    Update visionClient to use GOOGLE_CREDENTIALS_JSON env variable

--- server/utils/visionClient.js
+++ server/utils/visionClient.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const client = new vision.ImageAnnotatorClient({
-  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+  keyFilename: process.env.GOOGLE_CREDENTIALS_JSON,
 });
 
 export default client;
